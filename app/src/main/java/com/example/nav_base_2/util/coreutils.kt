a6fcc593af757bbfaa495fd296ad0f946ec0e6bd
junron <h1710074@nushigh.edu.sg>
2020-03-04 11:54:08 +0800

    Update kotlin

--- app/src/main/java/com/example/nav_base_2/util/coreutils.kt
+++ app/src/main/java/com/example/nav_base_2/util/coreutils.kt
@@ -1,7 +1,48 @@
 package com.example.nav_base_2.util
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 fun <T> MutableList<T>.removeAll() = this.removeAll(this)
 fun <T> MutableList<T>.setAll(list: List<T>) {
     removeAll()
     plusAssign(list)
 }
+
+
+fun String.truncate(length: Int, overflowIndicator: String = "...") =
+    if (this.length <= length) this
+    else this.substring(0, length - overflowIndicator.length) + overflowIndicator
+
+
+fun String.parseDate(
+    patterns: List<String> = listOf(
+        "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'",
+        "yyyy-MM-dd'T'HH:mm:ss'Z'",
+        "yyyy-MM-dd'T'HH:mm:ssX"
+    )
+): Date? {
+    for (pattern in patterns) {
+        try {
+            return SimpleDateFormat(pattern).parse(this)
+        } catch (e: ParseException) {
+        }
+    }
+    println("Fail: $this")
+    return null
+}
+
+
+fun String.toDp(numDp: Int) =
+    if (contains("."))
+        substringBefore(".") + "." +
+                substringAfter(".").substring(0 until numDp)
+    else "$this.0"
+
+
+fun Date.toStringValue(): String = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(this)
+fun String.toDate(): Date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(this)
+fun String.formatDate(): String = SimpleDateFormat("dd MMM yyyy").format(toDate())
+
+fun uuid() = UUID.randomUUID().toString()
