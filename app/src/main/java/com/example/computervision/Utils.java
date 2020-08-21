27c47a78ec5007765c29051839197f83ae95bfb8
JavierLimZH <32543880+JavierLimZH@users.noreply.github.com>
2020-08-22 04:42:42 +0800

    base cam

--- app/src/main/java/com/example/computervision/Utils.java
+++ app/src/main/java/com/example/computervision/Utils.java
@@ -6,14 +6,11 @@ import android.content.res.AssetFileDescriptor;
 import org.tensorflow.lite.Interpreter;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
-public class utils {
-    Interpreter loadModel(Activity activity) throws IOException {
+public class Utils {
+    public static Interpreter loadModel(Activity activity) throws IOException {
         AssetFileDescriptor fd = activity.getAssets().openFd("classifier.tflite");
         FileChannel fc = fd.createInputStream().getChannel();
         MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, fd.getStartOffset(), fd.getDeclaredLength());
