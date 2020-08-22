5c7e4cde2c191ce88df60ce4f7bac6f03c09c1a1
Lam Jun Rong <junron1@outlook.com>
2020-08-22 08:09:56 +0800

    Merge

--- app/src/main/java/com/example/shopeepee/fragments/ScanFragment.java
+++ app/src/main/java/com/example/shopeepee/fragments/ScanFragment.java
@@ -1,12 +1,13 @@
-package com.example.shopeepee;
+package com.example.shopeepee.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.shopeepee.R;
 
 /**
  * A simple {@link Fragment} subclass.
